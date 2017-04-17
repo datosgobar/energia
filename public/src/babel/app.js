accc286426e4b3ab636076998ee70a5bac9849e9
Francisco Tomás López <frammlopez@gmail.com>
2017-04-17 17:23:59 -0300

    Se avanza con maquetacion del sitio

--- public/src/babel/app.js
+++ public/src/babel/app.js
@@ -1,12 +1,9 @@
-let colores = ['#ffbb42','#808080', '#f1854b','#f2385a'];
-
-//  Variables Globales
-// let scaleColorLink  = d3.scaleOrdinal()
-//   .range('white')
-//   // .range(colores)
-//   .domain('Energías Primarias', 'Centros de Transformación', 'Energías Secundarias', 'Consumo');
-let fontScale       = d3.scaleLinear()
-  .range([8, 16]);
+// VARIABLES GLOBALES
+const colores = [
+  '#0075C9',
+  '#009DDA',
+  '#00B5E4'
+];
 
 //  Funciones
 // const formatoNumero = (d) => {
@@ -100,75 +97,53 @@ $(() => {
           .attr('stroke-width', 1.5)
           .attr('d', line);
       };
-      const updatePlaca = (element) => {
-        let contenedor = d3.select('#placa');
-            contenedor.select('.placa_titulo').style('text-transform', 'uppercase').text(element.name);
-            contenedor.select('.placa_prod').text(element.value);
-            contenedor.select('.placa_imp').text(element.value * 0.2);
-            contenedor.select('.placa_exp').text(element.value * 0.1);
-            contenedor.select('.placa_lost').text(element.value * 0.05);
-            contenedor.select('.placa_efi').text(((element.value -  (element.value * 0.05)) * 100) / element.value);
-            contenedor.select('.placa_more').text(element.value  * 0.01);
 
-        createGrafico(element.prod, contenedor.select('.placa_grafico.grafico_prod'));
-        createGrafico(element.imp, contenedor.select('.placa_grafico.grafico_imp'));
-        createGrafico(element.exp, contenedor.select('.placa_grafico.grafico_exp'));
-      };
+      const fadeIn = () => (g, i) => {
+        let all_links = d3.selectAll('#sankey .link'),
+            all_nodes = d3.selectAll('#sankey .node'),
+            nodes_id  = [];
 
-      const fadeIn = (opacity) => (g, i) => {
-        updatePlaca(g);
-
-        let id_nodes = [],
-            links,
-            linksFilter,
-            nodes,
-            nodesFilter;
-
-        // add event node
-        id_nodes.push(g.id);
-        // add sourceLinks
-        g.sourceLinks.forEach((link) => { id_nodes.push(link.target.id); });
-        // add targetLinks
-        g.targetLinks.forEach((link) => { id_nodes.push(link.source.id); });
-
-        // select links
-        links = d3.selectAll('#sankey .link');
-        //  links focusIn
-        // d3.selectAll('#sankey .link')
-        //   .filter((d) => (d.source.id === g.id || d.target.id === g.id))
-        //   .transition().style('stroke-opacity', .75);
-        // links focusOut
-        linksFilter = links.filter((d) => (d.source.id !== g.id && d.target.id !== g.id));
-        linksFilter.transition().style('stroke-opacity', opacity);
-
-        //  select nodes
-        nodes = d3.selectAll('#sankey .node');
-        // nodes focusIn
-        nodesFilter = nodes.filter((d) => {
-          let statusNode = false;
-
-          id_nodes.forEach((element) => { if (element === d.id) { statusNode = true; } });
-
-          return statusNode;
-        });
-        nodesFilter.select('rect').transition().style('fill', (d) => darken('white', 20));
-        // nodesFilter.select('rect').transition().style('fill', (d) => darken(colores[d.pos - 1], 20));
-        nodesFilter.select('text')
+        let event_node = all_nodes.filter((d) => (d.id === g.id));
+        event_node.select('rect')
           .transition()
-          .attr('text-anchor', 'middle')
-          .attr('y', -10)
-          // .text((d) => (d.name))
-          .attr('x', (d) => (d.dx / 2));
-        // nodes focusOut
-        nodesFilter = nodes.filter((d) => {
-          let statusNode = true;
+          .style('fill', 'black')
+          .style('stroke', 'black');
+        event_node.select('text')
+          .transition()
+          .style('fill', 'black');
 
-          id_nodes.forEach((element) => { if (element === d.id) { statusNode = false; } });
+        let filter_nodes = all_nodes.filter((d) => {
+          let state = false;
 
-          return statusNode;
+          nodes_id.forEach((element) => { if(d.id === element) { state = true; } });
+
+          return state;
         });
-        nodesFilter.transition().style('opacity', opacity);
-        };
+        filter_nodes.select('rect')
+          .transition()
+          .style('fill', 'black')
+          .style('stroke', 'black');
+        filter_nodes.select('text')
+          .transition()
+          .style('fill', 'black');
+
+        let filter_links = all_links.filter((d) => {
+          let state = true;
+
+          if (d.source.id === g.id) {
+            nodes_id.push(d.target.id);
+          } else if (d.target.id === g.id) {
+            nodes_id.push(d.source.id);
+          } else {
+            state = false;
+          }
+
+          return state;
+        });
+        filter_links.transition()
+          .style('stroke', '#0075C9')
+          .style('stroke-opacity', 0.5);        
+      };
       const fadeOut = (opacity) => (g, i) => {
 
         let id_nodes = [],
@@ -192,7 +167,7 @@ $(() => {
         //   .transition().style('stroke-opacity', .75);
         // links focusOut
         linksFilter = links.filter((d) => (d.source.id !== g.id && d.target.id !== g.id));
-        linksFilter.transition().style('stroke-opacity', 0.75);
+        linksFilter.transition().style('stroke-opacity', 0.25);
 
         //  select nodes
         nodes = d3.selectAll('#sankey .node');
@@ -261,48 +236,81 @@ $(() => {
       //   //   // actualizarSankey($('#content').width(), $('#content').height(), dataSankey, oldLinks);
       //   // }
       // };
-      const createBackButton = (node) => {
-        console.log(node);
-        let button = d3.select('#placa .agrup')
-          .append('div')
-            .attr('id', () => (`node_${ node.id }`))
-            .attr('class', 'backButton')
-            .on('click', (event) => {
-              if (nodesOri[node.id].group) {
-                nodesOri[node.id].group = false;
-              } else {
-                nodesOri[node.id].group = true;
-              }
 
-              $(`#node_${ node.id }`).remove();
-
-              preSankey();
-            });
-
-        button.append('span').attr('class', 'backButton_text').text(node.name);
-        button.append('span')
-          .append('i')
-          .attr('class', 'fa fa-times')
-          .attr('aria-hidden', 'true')
-          .style('margin-left', '10px');
-      };
       const dibujarSankey = (width, heigth, data) => {
 
-        // Variables
-        let margin = { top: 50, right: 50, bottom: 50, left: 50 };
-        let size = { width: (width - margin.left - margin.right), height: (heigth - margin.top - margin.bottom) };
-        let anchoNodo = 20;
-        let separacionNodo = 20;
-
+        // Se vacia contenedor
         $('#sankey').empty();
+
+        // Se definen variables
+        let margin = {
+          top: 40,
+          right: 200,
+          bottom: 40,
+          left: 200,
+          header: 20
+        },
+        headerSize = 19,
+        size = {
+          width: (((width < 1400)?(width):(1400)) - margin.left - margin.right),
+          height: (heigth - margin.top - margin.bottom - margin.header - headerSize)
+        },
+        secciones = [
+          'ENERGÍAS PRIMARIAS',
+          'PLANTAS DE TRATAMIENTO',
+          'ENERGÍAS SECUNDARIAS',
+          'CONSUMOS'
+        ],
+        anchoNodo = 20,
+        separacionNodo = 20;
 
         // Creación SVG
         svg = d3.select('#sankey')
           .append('svg')
-            .attr('width', size.width + margin.left + margin.right)
-            .attr('height', size.height + margin.top + margin.bottom)
-          .append('g')
-            .attr('transform', `translate(${ margin.left }, ${ margin.top })`);
+          .attr('width', size.width + margin.right + margin.left)
+          .attr('height', size.height + margin.top + margin.bottom + margin.header + headerSize);
+
+        // Borrar
+        // svg.append('g')
+        //   .attr('transform', `translate(${ 0 }, ${ 0 })`)
+        //   .append('rect')
+        //   .attr('width', `${ size.width + margin.right + margin.left }px`)
+        //   .attr('height', `${ margin.top }px`)
+        //   .style('fill', 'silver');
+        // Borrar
+        // svg.append('g')
+        //   .attr('transform', `translate(${ 0 }, ${ height - margin.bottom })`)
+        //   .append('rect')
+        //   .attr('width', `${ size.width + margin.right + margin.left }px`)
+        //   .attr('height', `${ margin.top }px`)
+        //   .style('fill', 'silver');
+        // Borrar
+        // svg.append('g')
+        //   .attr('transform', `translate(${ 0 }, ${ margin.top + headerSize })`)
+        //   .append('rect')
+        //   .attr('width', `${ size.width + margin.right + margin.left }px`)
+        //   .attr('height', `${ margin.header }px`)
+        //   .style('fill', 'silver');
+
+        // Se agregan encabezados
+        let encabezado = svg.append('g')
+          .attr('id', 'chart-encabezado')
+          .attr('transform', `translate(${ margin.left }, ${ margin.top + 15 })`);
+        secciones.forEach((v, k) => {
+          let anchoTotal = size.width + margin.right;
+
+          encabezado.append('text')
+            .attr('class', 'chart-encabezado-right')
+            .attr('x', (d) => (((anchoTotal / 4) * k) + ((anchoNodo / 2) * k) + anchoNodo + 10))
+            .text(v)
+            .filter((d) => (k === 0))
+            .attr('x', (d) => (((anchoTotal / 4) * k) + ((anchoNodo / 2) * k) - 10))
+            .attr('class', 'chart-encabezado-left');
+        });
+
+        // Se crea grafico
+        let chart = svg.append('g')
+          .attr('transform', `translate(${ margin.left }, ${ margin.top + $('#chart-encabezado')[0].getBBox().height + margin.header })`);
 
         // Creación Sankey
         sankeyChartD3 = d3.sankey()
@@ -313,122 +321,56 @@ $(() => {
           .links(data.links)
           .layout();
 
-        // const generarGradientes = (colores) => {
-        //   let keys = {};
-        //
-        //   colores.forEach((v, k) => {
-        //
-        //     colores.forEach((v2, k2) => {
-        //
-        //       keys[`${ k + 1 }${ k2 + 1 }`] = [v, v2];
-        //     });
-        //   });
-        //
-        //   return keys;
-        // };
-        //
-        // let keys = generarGradientes(colores);
-        //
-        // console.log(keys);
-        //
-        // for (let key in keys) {
-        //
-        //   eval(`let key_${ key } = svg.append('defs')
-        //     .append('linearGradient')
-        //       .attr('id', 'key_${ key }')
-        //       .attr('x1', '0%')
-        //       .attr('x2', '100%')
-        //       .attr('spreadMethod', 'pad');
-        //     key_${ key }.append('stop')
-        //         .attr('offset', '0%')
-        //         .attr('stop-color', '${ keys[key][0] }')
-        //         .attr('stop-opacity', 1);
-        //     key_${ key }.append('stop')
-        //         .attr('offset', '100%')
-        //         .attr('stop-color', '${ keys[key][1] }')
-        //         .attr('stop-opacity', 1);`);
-        // }
-
         // Creación de Links
         path = sankeyChartD3.link();
 
-        fontScale.domain(d3.extent(data.nodes, (d) => d.value));
-
         //  Se crean links
-        let link = svg.append('g')
+        let link = chart.append('g')
           .attr('id', 'links')
           .selectAll('.link')
           .data(data.links)
           .enter()
           .append('path')
-            .attr('class', 'link')
-            .attr('d', path)
-            .style('stroke-width', (d) => Math.max(1, d.dy))
-            // .style('stroke', 'silver')
-            // trabajando
-            .style('stroke', (d) => 'silver')
-            // .style('stroke', (d) => {
-            //   let key = [d.source.pos, d.target.pos];
-            //
-            //   return `url(#key_${ key[0] }${ key[1] })`;
-            // })
-            .style('stroke-opacity', 0.75)
-            .on('mouseover', (d) => { d3.select(d3.event.target).style('stroke-opacity', 1); })
-            .on('mouseout', (d) => { d3.select(d3.event.target).style('stroke-opacity', 0.75); })
-            .sort((a, b) => (b.dy - a.dy));
+          .attr('d', path)
+          .attr('class', 'link')
+          .style('stroke-width', (d) => Math.max(1, d.dy))
+          // .on('mouseover', (d) => { d3.select(d3.event.target).style('stroke-opacity', 1); })
+          // .on('mouseout', (d) => { d3.select(d3.event.target).style('stroke-opacity', 0.75); })
+          .sort((a, b) => (b.dy - a.dy));
 
-        link.filter((element) => (element.target.name === 'EXPORTACIONES' || element.target.name === 'CONSUMO PROPIO' || element.target.name === 'TRANSPORTE' || element.target.name === 'PERDIDA'))
-          .style('stroke', '#BA3F1D');
-        link.filter((element) => (element.source.name === 'IMPORTACIONES'))
-          .style('stroke', 'green');
+          // link.filter((element) => (element.target.name === 'EXPORTACIONES' || element.target.name === 'CONSUMO PROPIO' || element.target.name === 'TRANSPORTE' || element.target.name === 'PERDIDA'))
+          //   .style('stroke', '#BA3F1D');
 
         // Se crean nodos
-        let node = svg.append('g')
+        let node = chart.append('g')
           .attr('id', 'nodes')
           .selectAll('.node')
-          .data(data.nodes.filter((element) => (element.sourceLinks.length !== 0 || element.targetLinks.length !== 0)))
+          .data(data.nodes)
           .enter()
           .append('g')
             .attr('id', (d) => d.id)
             .attr('class', 'node')
-            .attr('transform', (d) => `translate(${ d.x }, ${ d.y })`)
-            .call(d3.drag().on('drag', dragmove));
+            .attr('transform', (d) => `translate(${ d.x }, ${ d.y })`); // error compatibilidad
+            // .call(d3.drag().on('drag', dragmove));
+
         // Se crean rectangulos nodos
         node.append('rect')
             .attr('width', sankeyChartD3.nodeWidth())
-            .attr('height', (d) => Math.max(10, d.dy))
-            .style('stroke', (d) => 'silver')
-            .style('fill', (d) => 'white')
-            // .style('fill', (d) => colores[d.pos - 1])
-            .on('mouseover', fadeIn(0.025))
-            .on('mouseout', fadeOut())
-            .on('dblclick', (e) => {
-
-              if (nodesOri[e.id].group) {
-                nodesOri[e.id].group = false;
-              } else {
-                nodesOri[e.id].group = true;
-              }
-              createBackButton(e);
-              preSankey();
-            });
+            .attr('height', (d) => Math.max(5, d.dy))
+            .on('mouseover', fadeIn())
+            .on('mouseout', fadeOut());
 
         // Se crean textos nodos
         node.append('text')
-            .attr('x', -10)
-            .attr('text-anchor', 'end')
-            .attr('y', (d) => (d.dy / 2))
-            .attr('dy', '.35em')
-            .style('fill', 'black')
-            // .style('fill', (d) => colores[d.pos - 1])
-            .style('font-size', '14px')
-            .style('font-weight', 'normal')
-            // .style('font-size', (d) => `${ Math.floor(fontScale(d.value)) }px`)
-            .text((d) => (d.name))
-            // .text((d) => (d.name.length > 8) ? (d.name.substring(0, 5) + '...') : (d.name))
-            .filter((d) => (d.x < width / 2))
-              .attr('x', 10 + sankeyChartD3.nodeWidth())
-              .attr('text-anchor', 'start');
+          .attr('class', 'node-text')
+          .attr('x', 10 + sankeyChartD3.nodeWidth())
+          .attr('y', (d) => (d.dy / 2))
+          .attr('dy', '0.35em')
+          .text((d) => (d.name))
+          .filter((d) => (d.targetLinks.length === 0))
+          .attr('class', 'node-text-start')
+          .attr('x', -10);
+
         // Se agrega texto referencia hover link
         link.append('title').text((d) => `${ d.source.name } (${ d.source.id }) → ${ d.target.name } (${ d.target.id }) → ${ d.value }`);
         // Se agrega texto referencia hover node
